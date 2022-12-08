18591ca6787a05c738a14d4347aba6773cc5948c
Dylan Stencil <35854396+dstencil@users.noreply.github.com>
2022-12-08 20:23:56 +0000

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -1,30 +1,14 @@
 from datetime import datetime
 import yfinance as yf
 
-stocks = ['AAPL', 'MSFT', 'AMD']
+
 
 def price_responses(input_text):
-    user_message = str(input_text).lower()
+    user_message = str(input_text).upper()
 
-    if user_message in (stocks):
-        msft = yf.Ticker(stocks)
-        price = msft.info['regularMarketPrice']
-        return(str(price))
-    
-    if user_message in ("aapl"):
-        msft = yf.Ticker("AAPL")
-        price = msft.info['regularMarketPrice']
+    if user_message:
+        ticker = yf.Ticker(str(user_message))
+        price = ticker.info['regularMarketPrice']
         return(str(price))
 
-    if user_message in ("msft"):
-        msft = yf.Ticker("MSFT")
-        price = msft.info['regularMarketPrice']
-        return(str(price))
-    
-     
-    if user_message in ("amd"):
-        msft = yf.Ticker("AMD")
-        price = msft.info['regularMarketPrice']
-        return(str(price))
-   
-    return "I dont understand..."
+    return "Please enter right symbol"
